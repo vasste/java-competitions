5f33756b5019aa9e5e46196ba84a0de7d890aece
Vasilii Stepanov <pinosol@gmail.com>
2017-11-09 22:51:46 +0300

    initial2

--- src/main/java/MyStrategy.java
+++ src/main/java/MyStrategy.java
@@ -1,19 +1,65 @@
 import model.*;
 
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
 public final class MyStrategy implements Strategy {
+
+    private static final Map<VehicleType, VehicleType[]> preferredTargetTypesByVehicleType;
+    static {
+        preferredTargetTypesByVehicleType = new EnumMap<>(VehicleType.class);
+
+        preferredTargetTypesByVehicleType.put(VehicleType.FIGHTER, new VehicleType[] {
+                VehicleType.HELICOPTER, VehicleType.FIGHTER
+        });
+
+        preferredTargetTypesByVehicleType.put(VehicleType.HELICOPTER, new VehicleType[] {
+                VehicleType.TANK, VehicleType.ARRV, VehicleType.HELICOPTER, VehicleType.IFV, VehicleType.FIGHTER
+        });
+
+        preferredTargetTypesByVehicleType.put(VehicleType.IFV, new VehicleType[] {
+                VehicleType.HELICOPTER, VehicleType.ARRV, VehicleType.IFV, VehicleType.FIGHTER, VehicleType.TANK
+        });
+
+        preferredTargetTypesByVehicleType.put(VehicleType.TANK, new VehicleType[] {
+                VehicleType.IFV, VehicleType.ARRV, VehicleType.TANK, VehicleType.FIGHTER, VehicleType.HELICOPTER
+        });
+    }
+
     @Override
     public void move(Player me, World world, Game game, Move move) {
-        if (world.getTickIndex() == 0) {
-            move.setAction(ActionType.CLEAR_AND_SELECT);
-            move.setRight(world.getWidth());
-            move.setBottom(world.getHeight());
-            return;
+        initializeTick(me, world, game, move);
+    }
+
+    private Player me;
+    private World world;
+    private Game game;
+    private Move move;
+    private final Map<Long, Vehicle> vehicleById = new HashMap<>();
+    private final Map<Long, Integer> updateTickByVehicleId = new HashMap<>();
+
+    private void initializeTick(Player me, World world, Game game, Move move) {
+        this.me = me;
+        this.world = world;
+        this.game = game;
+        this.move = move;
+
+        for (Vehicle vehicle : world.getNewVehicles()) {
+            vehicleById.put(vehicle.getId(), vehicle);
+            updateTickByVehicleId.put(vehicle.getId(), world.getTickIndex());
         }
 
-        if (world.getTickIndex() == 1) {
-            move.setAction(ActionType.MOVE);
-            move.setX(world.getWidth() / 2.0D);
-            move.setY(world.getHeight() / 2.0D);
+        for (VehicleUpdate vehicleUpdate : world.getVehicleUpdates()) {
+            long vehicleId = vehicleUpdate.getId();
+
+            if (vehicleUpdate.getDurability() == 0) {
+                vehicleById.remove(vehicleId);
+                updateTickByVehicleId.remove(vehicleId);
+            } else {
+                vehicleById.put(vehicleId, new Vehicle(vehicleById.get(vehicleId), vehicleUpdate));
+                updateTickByVehicleId.put(vehicleId, world.getTickIndex());
+            }
         }
     }
 }
